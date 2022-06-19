d545c14c5cef45ae3b5b377b3901bbb3415ca33b
Jorge de Paz <jorge.depazcabanas@gmail.com>
2022-06-20 01:03:47 +0200

    new components & services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularyComponent } from './components/formulary/formulary.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularyComponent,
+    HomeComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
