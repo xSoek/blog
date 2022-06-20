a570b5f422d37c70d309c4b90b2d2a4187a261d4
Jorge de Paz <jorge.depazcabanas@gmail.com>
2022-06-20 20:01:32 +0200

    add post to array finished

---
+++ src/app/dbs/posts.db.ts
@@ -0,0 +1,12 @@
+import { Post } from '../interfaces/post';
+
+export const POSTS: Post[] = [
+    {
+        _id: 0,
+        author: "Jorge de Paz Cabañas",
+        date: "1 de junio de 2022, 19:40",
+        title: "Los lobos molan",
+        category: "Montaña",
+        image: "https://static.nationalgeographic.es/files/styles/image_3200/public/wolf-635063_1920.jpg",
+    },
+]
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { POSTS } from '../dbs/posts.db';
 import { Post } from '../interfaces/post';
 
 @Injectable({
@@ -6,11 +7,32 @@ import { Post } from '../interfaces/post';
 })
 export class PostsService {
 
-  arrPosts: Post[] = [];
-  constructor() { }
+  monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
 
-  addPost(newPost: Post) {
-    this.arrPosts.push(newPost)
+  arrPosts: Post[] = [];
+  constructor() {
+    this.arrPosts = POSTS;
+  }
+
+  addPost(newPost: any) {
+    const currentDate = new Date();
+    let date = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDay(),
+      currentDate.getHours(),
+      currentDate.getMinutes()
+    )
+
+    const finalDate = date.toLocaleString('default', { month: 'long', year: "numeric", day: 'numeric', hour: '2-digit', minute: '2-digit' });
+
+    newPost['date'] = finalDate;
+    newPost._id = this.arrPosts.length;
+    this.arrPosts.push(newPost);
+    console.log(this.arrPosts);
+
     return true;
   }
 
