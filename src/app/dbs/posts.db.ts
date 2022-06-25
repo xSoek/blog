5529cd5602b8934990c4a4a141d7239d5e684249
Jorge de Paz <jorge.depazcabanas@gmail.com>
2022-06-26 00:20:39 +0200

    categoryViewComponent structure and styled

--- src/app/dbs/posts.db.ts
+++ src/app/dbs/posts.db.ts
@@ -9,4 +9,28 @@ export const POSTS: Post[] = [
         category: 0,
         image: "https://static.nationalgeographic.es/files/styles/image_3200/public/wolf-635063_1920.jpg",
     },
+    {
+        _id: 1,
+        author: "Jorge de Paz Cabañas",
+        date: "1 de junio de 2022, 19:40",
+        title: "Los lobos molan",
+        category: 0,
+        image: "https://static.nationalgeographic.es/files/styles/image_3200/public/wolf-635063_1920.jpg",
+    },
+    {
+        _id: 2,
+        author: "Jorge de Paz Cabañas",
+        date: "1 de junio de 2022, 19:40",
+        title: "Los lobos molan",
+        category: 0,
+        image: "https://static.nationalgeographic.es/files/styles/image_3200/public/wolf-635063_1920.jpg",
+    },
+    {
+        _id: 3,
+        author: "Jorge de Paz Cabañas",
+        date: "1 de junio de 2022, 19:40",
+        title: "Los lobos molan",
+        category: 0,
+        image: "https://static.nationalgeographic.es/files/styles/image_3200/public/wolf-635063_1920.jpg",
+    },
 ]
