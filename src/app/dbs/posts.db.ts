5685c55e414f304867059c32460ef5a3e394c978
Jorge de Paz <jorge.depazcabanas@gmail.com>
2022-06-27 00:23:14 +0200

    Hotfix interface added to the database info

--- src/app/dbs/posts.db.ts
+++ src/app/dbs/posts.db.ts
@@ -4,6 +4,7 @@ export const POSTS: Post[] = [
     {
         _id: 0,
         author: "Jorge de Paz Cabañas",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem asperiores optio necessitatibus sapiente. Esse, ad dolores molestiae velit dicta aperiam excepturi alias inventore. Praesentium reprehenderit quae, eum natus facilis possimus harum necessitatibus deserunt asperiores. Accusantium aut illum consequuntur voluptates fugiat veritatis quas aperiam assumenda distinctio quos itaque iste, vitae nulla, quae ad doloribus tempora quod reprehenderit mollitia cumque facilis, esse delectus voluptas praesentium. Quis totam ipsa ducimus sed itaque est vel. Tenetur corporis dolorem explicabo sequi facere fuga similique neque assumenda sed numquam, odio ex vero iusto modi? Voluptate molestias adipisci quis commodi et qui quo nihil quibusdam voluptatum inventore!",
         date: "1 de junio de 2022, 19:40",
         title: "Los lobos molan",
         category: 0,
@@ -12,6 +13,7 @@ export const POSTS: Post[] = [
     {
         _id: 1,
         author: "Jorge de Paz Cabañas",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem asperiores optio necessitatibus sapiente. Esse, ad dolores molestiae velit dicta aperiam excepturi alias inventore. Praesentium reprehenderit quae, eum natus facilis possimus harum necessitatibus deserunt asperiores. Accusantium aut illum consequuntur voluptates fugiat veritatis quas aperiam assumenda distinctio quos itaque iste, vitae nulla, quae ad doloribus tempora quod reprehenderit mollitia cumque facilis, esse delectus voluptas praesentium. Quis totam ipsa ducimus sed itaque est vel. Tenetur corporis dolorem explicabo sequi facere fuga similique neque assumenda sed numquam, odio ex vero iusto modi? Voluptate molestias adipisci quis commodi et qui quo nihil quibusdam voluptatum inventore!",
         date: "1 de junio de 2022, 19:40",
         title: "Los lobos molan",
         category: 0,
@@ -20,6 +22,7 @@ export const POSTS: Post[] = [
     {
         _id: 2,
         author: "Jorge de Paz Cabañas",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem asperiores optio necessitatibus sapiente. Esse, ad dolores molestiae velit dicta aperiam excepturi alias inventore. Praesentium reprehenderit quae, eum natus facilis possimus harum necessitatibus deserunt asperiores. Accusantium aut illum consequuntur voluptates fugiat veritatis quas aperiam assumenda distinctio quos itaque iste, vitae nulla, quae ad doloribus tempora quod reprehenderit mollitia cumque facilis, esse delectus voluptas praesentium. Quis totam ipsa ducimus sed itaque est vel. Tenetur corporis dolorem explicabo sequi facere fuga similique neque assumenda sed numquam, odio ex vero iusto modi? Voluptate molestias adipisci quis commodi et qui quo nihil quibusdam voluptatum inventore!",
         date: "1 de junio de 2022, 19:40",
         title: "Los lobos molan",
         category: 0,
@@ -28,6 +31,7 @@ export const POSTS: Post[] = [
     {
         _id: 3,
         author: "Jorge de Paz Cabañas",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem asperiores optio necessitatibus sapiente. Esse, ad dolores molestiae velit dicta aperiam excepturi alias inventore. Praesentium reprehenderit quae, eum natus facilis possimus harum necessitatibus deserunt asperiores. Accusantium aut illum consequuntur voluptates fugiat veritatis quas aperiam assumenda distinctio quos itaque iste, vitae nulla, quae ad doloribus tempora quod reprehenderit mollitia cumque facilis, esse delectus voluptas praesentium. Quis totam ipsa ducimus sed itaque est vel. Tenetur corporis dolorem explicabo sequi facere fuga similique neque assumenda sed numquam, odio ex vero iusto modi? Voluptate molestias adipisci quis commodi et qui quo nihil quibusdam voluptatum inventore!",
         date: "1 de junio de 2022, 19:40",
         title: "Los lobos molan",
         category: 0,
